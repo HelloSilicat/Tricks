8e38c85626c107d091dd542a4805965c5f6d4313
Jialiang Pei <453715647pjl@gmail.com>
2018-05-07 09:46:12 +0800

    增加定时发送功能

--- ketangpai/Boom.py
+++ ketangpai/Boom.py
@@ -1,108 +1,44 @@
-import json
+import random
+import datetime
+import time
 import requests
 from bs4 import BeautifulSoup
 from urllib import request
 from urllib.error import HTTPError
 
-def input_manual(mode = 1):
-    title = input("Title:\n>")
-    author = input("Author:\n>")
-    item = []
-    number = int(input("Number:\n>"))
-    for i in range(number):
-        print("Add %dth items......\n"%(i + 1))
-        dic = {}
-        dic["Vocabulary"] = input("Vocabulary:\n>")
-        dic["Definition"] = input("Definition:\n>")
-        dic["Example"] = input("Example:\n>")
-        dic["Remark"] = input("Remark:\n>")
-        item.append(dic)
-    with open("temp.json","a") as outfile:
-        json.dump(item, outfile, ensure_ascii=False)
-        outfile.write('\n')
-    return item,title,author
-
 def outfile(item,title,author):
     number = len(item)
     dot = "<b>·</b>"
     content = ""
-    # add title
     content = content + "<h3><i>{0}-{1}</i></h3>\n".format(title, author)
-    # add items
     for i in range(number):
-        # add Vocabulary
-        #print("debug info")
         content = content + "<b>{0}. {1}</b>\n".format(i+1, item[i]["Vocabulary"])
-        # add Definition
         content = content + dot + "Definition: {0}\n".format(item[i]["Definition"])
-        # add Example
         content = content + dot + "Example: {0}\n".format(item[i]["Example"])
-        # add Remark
-        if (item[i]["Remark"] != "None"):
-            content = content + dot + "Remark: {0}\n".format(item[i]["Remark"])
-    # outfile
-    with open("Look here.txt","a",encoding="utf-8") as file:
+    with open("pei.txt","w",encoding="utf-8") as file:
         file.write(content)
         print("Finished.\n")
 
 def auto_gernerate_single():
     root_url = "http://dict.youdao.com/w/eng/"
-    voc = input("What is your vocabulary?\n>").strip().replace(" ","_")
-    url = root_url + voc
-    print("debug info:",url)
+    voclist = BeautifulSoup(request.urlopen("http://dict.yqie.com/GRE_glossary.htm"),"lxml").findAll("p")
+    voc = voclist[random.randint(0,len(voclist) - 1)].text
+    print(voc)
     dic = {}
-    # get the whole page of the voc
-    flag = False
-    while (not flag):
-        try:
-            flag = True
-            soup = BeautifulSoup(request.urlopen(url),"lxml")
-        except HTTPError as e:
-            print("This vocabulary is missing! Why not change another one?")
-            voc = input("What is your vocabulary?\n>").strip().replace(" ","_")
-            url = root_url + voc
-            print("debug info:",url)
-            flag = False
-    # get the definition of the voc
-    flag = False
-    while (not flag):
-        try:
-            flag = True
-            trans_container = soup.find("div",{"class":"trans-container"})
-        except HTTPError as e:
-            print("ERROR: trans_container is not found!")
-            flag = False
+    soup = BeautifulSoup(request.urlopen(root_url+voc),"lxml")
+    trans_container = soup.find("div",{"class":"trans-container"})
     Definition = str(trans_container.find("ul").text).replace("\n","  ")
     # get the example of the voc
-    flag = False
-    while (not flag):
-        try:
-            flag = True
-            examplesToggle = soup.find("div",{"id":"examplesToggle"})
-        except HTTPError as e:
-            print("ERROR: examplesToggle is not found!")
-            flag = False
+    examplesToggle = soup.find("div",{"id":"examplesToggle"})
     temp = examplesToggle.find("li").findAll("p")
     Example = (str(temp[0].text) + str(temp[1].text)).replace("\n"," ")
-    # get the remark of the voc
-    print("Definition: ", Definition)
-    print("Example: ", Example)
-    s = input("Do you need a remark?(Y/N)").upper().strip()
-    while (not (s == 'Y' or s == 'N')):
-        print("Your input is invalid, please input again.")
-        s = input("Do you need a remark?(Y/N)").upper().strip()
-    if (s == 'Y'):
-        Remark = input("What is your remark?\n>").strip()
-    else :
-        Remark = "None"
     dic["Vocabulary"] = voc.replace("_"," ")
     dic["Definition"] = Definition
     dic["Example"] = Example
-    dic["Remark"] = Remark
     return dic
 
 def auto_gernerate(title, author):
-    number = int(input("Number:\n>"))
+    number = 2
     item = []
     for i in range(number):
         dic = auto_gernerate_single()
@@ -112,28 +48,26 @@ def auto_gernerate(title, author):
 def sent(filename):
     header = {    
         'User-Agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-        'Cookie':'***********'
-    }
+        'Cookie':''
     datas = {
         'content':'',
         'topicid':''
     }
     content = open(filename, "r",encoding="utf-8").read()
-    topicid = "MDAwMDAwMDAwMLOGpZmHz7Nr"
+    topicid = ""
     datas["content"]=content
     datas["topicid"]=topicid
     response = requests.post("https://www.ketangpai.com/TopicDiscussApi/addDiscuss", headers=header, data=datas).content
     print("Submit is successful!")
 
 
-title = input("Title:\n>")
-author = input("Author:\n>")
-# item,title,author = input_manual(1)
-#outfile(item,title,author)
-auto_gernerate(title, author)
-input("Click anything to submit!\n")
-sent("Look here.txt")
-    
+#Pei
+while(True):
+    t = datetime.datetime.now()
+    if (t.hour == 10 and t.minute == 30):
+        time.sleep(60)
+        auto_gernerate("The Call of the Wild", "Jack London")
+        sent("pei.txt") 
 
 
 
